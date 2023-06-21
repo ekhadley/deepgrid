83047f3fda3b38ac0ef5b666ecc8a111ed9a3011
Ethan_Hadley <ekhadley@gmail.com>
2023-06-21 13:02:26 -0500

    finished grid class which now has an observation state that can be fed into the model. Started on the nn architecture

--- train.py
+++ train.py
@@ -1,22 +1,17 @@
-
-import cv2, time, numpy as np
+import time
 from tqdm import tqdm
 from funcs import *
 from agent import *
 from grid import *
 
-g = grid((8, 5), numFood=5, numBomb=5)
+g = grid((8, 5), numFood=6, numBomb=7)
 a = agent(g)
 
-#for i in tqdm(range(10000)):
-i = g.view()
-cv2.imshow("g", i)
-cv2.waitKey(1)
-while 1:
-    #g.takeAction(random.randint(0,4))
-    a.userAction()
-    print(g.tiles)
+for i in tqdm(range(500000)):
+#while 1:
+    a.randomAction()
+    g.printObs()
 
-    i = g.view()
-    cv2.imshow("g", i)
-    cv2.waitKey(1)
+    #i = g.view()
+    #cv2.imshow("g", i)
+    #cv2.waitKey(1)
