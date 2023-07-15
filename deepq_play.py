0b673b024e84fb49862d21b4f9203398dbd143ec
Ethan_Hadley <ekhadley@gmail.com>
2023-07-15 15:12:45 -0500

    small syntax change

--- deepq_play.py
+++ deepq_play.py
@@ -9,7 +9,8 @@ g = grid((8, 5), numFood=12, numBomb=12)
 a = agent(g)
 
 
-startVersion = 32800
+startVersion = 800
+#loadDir = f"D:\\wgmn\\deepgrid\\deepq_net"
 loadDir = f"D:\\wgmn\\deepgrid\\netx\\{startVersion}"
 a.load(loadDir)
 
@@ -25,22 +26,21 @@ a.load(loadDir)
 a.epsilon = 0
 epscores = []
 #while 1:
-for i in trange(300, ncols=100, desc=cyan, unit="ep"):
+for i in trange(300, ncols=100, desc=purple, unit="ep"):
     while not g.terminate:
-        #reward = a.doRandomAction()
         state = g.observe()
-        action, pred, wasRandom = a.chooseAction(state)
-        reward = a.doAction(action, store=False)
+        action, pred = a.chooseAction(state)
+        reward = a.doAction(action)
         #print(f"taking action {yellow}{action}{endc} gave a reward of {purple}{reward:.2f}{endc}. The agent now has a score of {cyan}{a.score:.2f}{endc} on step {g.stepsTaken}/{g.maxSteps}")
         #print(f"{yellow}{pred=}{endc}")
         
-        im = g.view()
-        cv2.imshow("grid", im)
-        cv2.waitKey(100)
+        #im = g.view()
+        #cv2.imshow("grid", im)
+        #cv2.waitKey(50)
         #print(g)
 
     g.reset()
     epscores.append(a.reset())
 
 ascore = np.mean(epscores)
-print(f"{cyan}{ascore=}{endc}")
+print(f"{blue}{ascore=}{endc}")
