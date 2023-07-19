7d5692f899e9dd74ccca71843147f608f92b8e29
Ethan_Hadley <ekhadley@gmail.com>
2023-07-19 18:56:11 -0500

    imports

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import cv2, numpy as np
-from tinygrad.nn import Tensor
+from tqdm import trange
+import math
 
 purple = '\033[95m'
 blue = '\033[94m'
@@ -11,6 +12,15 @@ endc = '\033[0m'
 bold = '\033[1m'
 underline = '\033[4m'
 
+def sampleDist(probs, returnProb=False):
+    assert round(sum(probs), 3) <= 1, f"distribution probabilities should sum to ~1"
+    r = np.random.uniform(0, 1)
+    cum = [sum(probs[0:i+1]) for i in range(len(probs))]
+    for i, c in enumerate(cum):
+        if r < c:
+            if returnProb: return i, probs[i]
+            return i
+
 def imscale(img, s):
     try:
         w, h, d = np.shape(img)
@@ -18,13 +28,3 @@ def imscale(img, s):
         w, h = np.shape(img)
     assert not 0 in [w, h], "empty src image"
     return cv2.resize(img, (round(len(img[0])*s), round(len(img)*s)), interpolation=cv2.INTER_NEAREST)
-
-def isint(val):
-    return isinstance(val, (int, np.integer))
-
-def sampleDist(probs):
-    assert sum(probs) <= 1, f"distribution probabilities should sum to 1"
-    r = np.random.uniform(0, 1)
-    cum = [sum(probs[0:i+1]) for i in range(len(probs))]
-    for i, c in enumerate(cum):
-        if r < c: return i
