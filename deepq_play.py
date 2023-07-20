e7f047bd8e3359b01e207c8f42b1d718ad13a3da
Ethan_Hadley <ekhadley@gmail.com>
2023-07-19 19:06:00 -0500

    changed to pytorch

--- deepq_play.py
+++ deepq_play.py
@@ -14,6 +14,8 @@ startVersion = 100000
 loadDir = f"D:\\wgmn\\deepgrid\\deepq_100k.pth"
 a.load(loadDir)
 
+show = True
+prnt = True
 a.epsilon = 0
 epscores = []
 #while 1:
@@ -21,14 +23,17 @@ for i in (t:=trange(1000, ncols=100, desc=purple, unit="ep")):
     while not g.terminate:
         state = g.observe()
         action, pred = a.chooseAction(state)
-        reward = a.doAction(action)
+        #reward = a.doAction(action)
+        reward = a.doUserAction()
+        
         #print(f"taking action {yellow}{action}{endc} gave a reward of {purple}{reward:.2f}{endc}. The agent now has a score of {cyan}{a.score:.2f}{endc} on step {g.stepsTaken}/{g.maxSteps}")
         #print(f"{green}{pred=}{endc}")
         
-        #im = g.view()
-        #cv2.imshow("grid", im)
-        #cv2.waitKey(50)
-        #print(g)
+        if show:
+            im = g.view()
+            cv2.imshow("grid", im)
+            cv2.waitKey(50)
+        if prnt: print(g)
 
     g.reset()
     epscores.append(a.reset())
