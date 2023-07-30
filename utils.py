3e6d1bdf7acb3c727041a19e87bf440b0b67246e
Ethan_Hadley <ekhadley@gmail.com>
2023-07-30 09:23:03 -0500

    more colors!

--- utils.py
+++ utils.py
@@ -3,12 +3,17 @@ import cv2, numpy as np
 purple = '\033[95m'
 blue = '\033[94m'
 cyan = '\033[96m'
-green = '\033[92m'
+lime = '\033[92m'
 yellow = '\033[93m'
-red = '\033[91m'
-endc = '\033[0m'
+red = "\033[38;5;196m"
+pink = "\033[38;5;206m"
+orange = "\033[38;5;202m"
+green = "\033[38;5;34m"
+gray = "\033[38;5;8m"
+
 bold = '\033[1m'
 underline = '\033[4m'
+endc = '\033[0m'
 
 def sampleDist(probs, returnProb=False):
     summ = sum(probs)
