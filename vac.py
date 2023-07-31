028d6277618cec07e392d91c0b9b7b78f6e678bf
Ethan_Hadley <ekhadley@gmail.com>
2023-07-31 11:00:03 -0500

    valnet trains like the qlearner now

--- vac.py
+++ vac.py
@@ -8,6 +8,7 @@ from utils import *
 from env import grid
 import agent, vpo
 import wandb
+import cProfile
 
 class valueNet(agent.valnet):
     def loss(self, vals, rewards, nstates, terminal, discount=1, debug=False):
@@ -36,18 +37,20 @@ class valueNet(agent.valnet):
 
 
 class vacAgent(agent.agent):
-    def __init__(self, env, stepCost=0, actions=4, policyLr=0.001, valnetLr=0.001):
+    def __init__(self, env, stepCost=0, actions=4, policyLr=0.001, valnetLr=0.001, maxMemory=1000):
         self.numActions = actions
         self.env = env
         self.score = 0
         self.stepCost = stepCost
+        self.maxMemory = maxMemory
         self.policy = vpo.PolicyNet(self.env.size, 4, lr=policyLr) #we borrow the policy net architecture from our vpo implementation
         self.target = valueNet(self.env.size, lr=valnetLr)
         self.main = valueNet(self.env.size, lr=self.target.lr)
         self.update()
         # policy updates need states, actions, weights from the valnet
         # valnet updates need states, rewards, next states, terminal_state
-        self.memory = tuple([[] for i in range(6)])
+        self.policymem = [[], [], []]
+        self.valnetmem = [[], [], [], []]
 
     def chooseAction(self, state, greedy=False):
         #if not isinstance(state, Tensor): st = Tensor(state).reshape((1, *state.shape))
@@ -59,32 +62,44 @@ class vacAgent(agent.agent):
         return action, dist
 
     def remember(self, state, action, reward, weight, nextState, terminal):
-        self.memory[0].append(state)
-        self.memory[1].append(action)
-        self.memory[2].append(reward)
-        self.memory[3].append(weight)
-        self.memory[4].append(nextState)
-        self.memory[5].append(terminal)
+        self.policymem[0].append(state)
+        self.policymem[1].append(action)
+        self.policymem[2].append(weight)
 
-    def train(self):
-        states, actions, rewards, weights, nstates, terminals = self.memory
-        states = torch.Tensor(np.float32(states))
-        actions = torch.Tensor(np.float32(actions))
-        weights = torch.Tensor(np.float32(weights))
-        rewards = torch.Tensor(np.float32(rewards))
-        nstates = torch.Tensor(np.float32(nstates))
-        terminals = torch.Tensor(np.float32(terminals))
-        pLoss = self.trainPolicy(states, actions, weights, debug=False)
-        vLoss = self.trainValnet(states, rewards, nstates, terminals, debug=False)
-        return pLoss, vLoss
-    
-    def trainPolicy(self, states, actions, weights, debug=False):
+        self.valnetmem[0].append(state)
+        self.valnetmem[1].append(reward)
+        self.valnetmem[2].append(nextState)
+        self.valnetmem[3].append(terminal)
+        if len(self.valnetmem[0]) > self.maxMemory:
+            over = len(self.valnetmem[0]) - self.maxMemory
+            del self.valnetmem[0][:over]
+            del self.valnetmem[1][:over]
+            del self.valnetmem[2][:over]
+            del self.valnetmem[3][:over]
+
+    def trainPolicy(self, debug=False):
+        states = torch.tensor(np.float32(self.policymem[0]))
+        actions = torch.tensor(np.float32(self.policymem[1]))
+        weights = torch.tensor(np.float32(self.policymem[2]))
         dists, loss = self.policy.train(states, actions, weights, debug=debug)
-        return loss
-    def trainValnet(self, states, rewards, nstates, terminals, debug=True):
+        return dists, loss
+
+    def trainValnet(self, experience, debug=False):
+        states, rewards, nstates, terminals = experience
         vals, loss = self.target.train(states, rewards, nstates, terminals, debug=debug)
-        return loss
-    
+        return vals, loss
+    def sampleMemory(self, num, tensor=True):
+        assert len(self.valnetmem[1]) > num, f"{red}{bold}requested sample size of {num} but only have {len(self.valnetmem[1])} experiences{endc}"
+        samp = np.random.randint(0, len(self.valnetmem[0]), size=(num))
+        
+        expSample = [[] for i in range(4)]
+        for s in samp:
+            for i, mem in enumerate(expSample):
+                mem.append(self.valnetmem[i][s])
+        if tensor:
+            for i, mem in enumerate(expSample):
+                expSample[i] = torch.tensor(np.float32(expSample[i]))
+        return tuple(expSample)
     def getVal(self, state):
         if isinstance(state, np.ndarray): state = torch.from_numpy(state)
         #if state.device != "cuda": state = state.to("cuda")
@@ -106,7 +121,7 @@ class vacAgent(agent.agent):
         self.score = 0
         return s
     def forget(self):
-        self.memory = tuple([[] for i in range(6)])
+        self.policymem = [[], [], []]
 
 def train(show=False,
           save=None,
@@ -115,11 +130,13 @@ def train(show=False,
           trainEvery = 30,
           switchEvery = 3,
           numEpisodes = 15_001,
+          batchSize = 32,
+          maxMemory=1000,
           policyLr = 0.001,
           valnetLr = 0.001):
 
     g = grid((8, 5), numFood=12, numBomb=12)
-    a = vacAgent(g, policyLr=policyLr, valnetLr=valnetLr)
+    a = vacAgent(g, policyLr=policyLr, valnetLr=valnetLr, maxMemory=maxMemory)
     
     wandb.init(project="vac")
     wandb.watch(a.policy, a.target, log="all")
@@ -127,6 +144,7 @@ def train(show=False,
         print(f"{green}attemping load from {loadDir}{endc}")
         a.load(loadDir) 
 
+    trainingStart = 2*batchSize//g.maxSteps
     epscores, losses = [], []
     for i in (t:=trange(numEpisodes, ncols=120, unit="ep")):
         ep = i + startVersion
@@ -151,23 +169,23 @@ def train(show=False,
                 d = np.array_str(dist.detach().numpy(), precision=3, suppress_small=True)
                 print(f"{bold}{yellow}dist={d}, {green}val={ival:.2f}, {red}{weight} = {reward} + {vnext}{endc}")
 
-        #try: print(f"\n{blue}{weight} = {green}{reward} + {yellow}{tdiff}{endc}")
-        #except: pass
         g.reset()
         epscore = a.reset()
         epscores.append(epscore)
-        if i != 0 and i%(switchEvery*trainEvery)==0: a.update()
+        if i >= trainingStart:
+            experience = a.sampleMemory(batchSize)
+            vals, vLoss = a.trainValnet(experience)
+            if i%switchEvery==0: a.update()
         if i != 0 and i%trainEvery==0:
-            pLoss, vLoss = a.train()
+            dists, pLoss = a.trainPolicy()
             a.forget()
             
             recents = np.mean(epscores[-200:-1])
             d = np.array_str(dist.detach().numpy(), precision=3, suppress_small=True)
             desc = f"{bold}{blue}scores:{recents:.2f}, {blue}dist={d}, {green}val={ival:.2f} {endc}{blue}"
             t.set_description(desc)
-            
             wandb.log({"epscore": epscore, "policy_loss":pLoss, "valnet_loss":vLoss, "val":ival, "score":recents})
-        if i%saveEvery == 0:
+        if save is not None and i%saveEvery == 0:
             name = f"net_{ep}"
             a.save(save, name)
 
@@ -176,12 +194,18 @@ def play(load, show=False):
     a = vacAgent(g)
     agent.play(a, g, load=load, show=show)
 
-startVersion = 200000
+startVersion = 0
 loadDir = f"D:\\wgmn\\deepgrid\\vac_net_new\\net_{startVersion}"
 #loadDir = f"D:\\wgmn\\deepgrid\\vac_80k"
 saveDir = f"D:\\wgmn\\deepgrid\\vac_net_new"
 
+#prof = cProfile.Profile()
+#prof.enable()
+
 if __name__ == "__main__":
-    play(load=loadDir, show=False)
-    #train(load=None, save=saveDir, valnetLr=0.012, policyLr=0.0012, trainEvery=50, switchEvery=5, numEpisodes=100_001, show=False)
+    #play(load=loadDir, show=False)
+    train(load=None, save=None, valnetLr=0.02, policyLr=0.001, batchSize=32, trainEvery=40, switchEvery=3, maxMemory=300, numEpisodes=100_001, show=False)
     #sweep()
+
+#prof.disable()
+#prof.dump_stats("tmp")
